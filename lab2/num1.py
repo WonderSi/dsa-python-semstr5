579d40dc346352ee7ab574d8e6b204667d287da8
WonderSi <kirillkozlov00@gmail.com>
2025-09-17 22:01:10 +0700

    complete num_three in num1

--- lab2/num1.py
+++ lab2/num1.py
@@ -36,9 +36,34 @@ def num_two():
     
     plt.show()
 
+def num_three():
+
+    x1 = [10, 20, 30]
+    y1 = [20, 40, 10]
+
+    x2 = [10, 20, 30]
+    y2 = [40, 10, 30]
+
+    plt.plot(x1, y1, color='blue', linewidth=1, linestyle='dotted', label='line1-dotted')
+    plt.plot(x2, y2, color='red', linewidth=5, linestyle=':', label='line2-dashed')
+
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+    plt.title('Plot with two or more lines with different styles')
+    plt.legend()
+
+    plt.xlim(10, 30)
+    plt.ylim(10, 40)
+
+    plt.xticks([10, 15, 20, 25, 30])
+
+    plt.show()
+
+
 
 if __name__ == '__main__':
     pass
 
     # num_one()
-    num_two()
+    # num_two()
+    num_three()
